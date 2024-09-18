f9b6be646a1d07d74980f7e6cf73405157c1b278
K3NTUB3 <kentuckymachenzie@gmail.com>
2024-09-18 10:00:13 +0800

    Altered the activity of Lesson 10 to fit the activity created in the YT tutorial.

--- CrashCourse/L10Function.cs
+++ CrashCourse/L10Function.cs
@@ -13,8 +13,9 @@ namespace CrashCourse
         {
             string name, school, course;
             int age;
+            float grade;
 
-            Console.WriteLine("\nLet's Store your Details!\n\n");
+            Console.WriteLine("\nSTUDENT DETAILS\n\n");
             Console.Write("Enter Name    : ");
             name = Console.ReadLine();
 
@@ -27,27 +28,62 @@ namespace CrashCourse
             Console.Write("Enter Course  : ");
             course = Console.ReadLine();
 
-            Product product = new Product(name, age, school, course);
+            Console.Write("Enter Grade   : ");
+            grade = Convert.ToSingle(Console.ReadLine());
+
+            Student stud = new Student(name, age, school, course, grade);
+
+            stud.IntroduceSelf();
+            Console.WriteLine();
+            stud.EvaluateGrade();
+
         }
 
     }
-    class Product
+    class Student
     {
         public string name, school, course;
         public int age;
+        public float grade;
 
-        public Product(string name, int age, string school, string course)
+        public Student(string name, int age, string school, string course, float grade)
         {
             this.name = name;
             this.age = age;
             this.school = school;
             this.course = course;
+            this.grade = grade;
 
             Console.WriteLine("\n\nPerson Details Created!\n");
-            Console.WriteLine("Name    : " + name);
-            Console.WriteLine("Age     : " + age);
-            Console.WriteLine("School  : " + school);
-            Console.WriteLine("Course  : " + course);
+        }
+        public void IntroduceSelf()
+        {
+            Console.WriteLine("Hi! My name is " + name + ". I am " +
+                age + " years old. " + "\nI am a student of " + school +
+                ", currently taking " + course + " course.");
+        }
+        public void EvaluateGrade()
+        {
+            if (grade >= 99)
+            {
+                Console.WriteLine("WITH HIGHEST HONORS!\n");
+            }
+            else if (grade >= 95)
+            {
+                Console.WriteLine("WITH HIGH HONORS!\n");
+            }
+            else if (grade >= 90)
+            {
+                Console.WriteLine("WITH HONORS!\n");
+            }
+            else if (grade >= 75)
+            {
+                Console.WriteLine("Passed!\n");
+            }
+            else 
+            {
+                Console.WriteLine("Failed!\n");
+            }
 
         }
     }
